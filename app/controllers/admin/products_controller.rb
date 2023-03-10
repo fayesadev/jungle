8a511dac0e60a95e626640096ef7bf9ad4884b76
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-03-09 21:00:00 -0700

    Edit admin products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   def index
     @products = Product.order(id: :desc).all
   end
