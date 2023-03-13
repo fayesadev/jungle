8dd344ef60d8eb28cda46a0ce9ec6d29403105f2
fayesadev <fayedumbrigue@gmail.com>
2023-03-13 22:32:15 +0000

    Complete testing for product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -30,6 +30,41 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
+    it "shouldn't save when price is nil" do
+      @product = Product.new(
+        name: "Money Tree",
+        price_cents: nil,
+        quantity: 50,
+        category: @category
+      )
 
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "shouldn't save when quantity is nil" do
+      @product = Product.new(
+        name: "Money Tree",
+        price_cents: 1499,
+        quantity: nil,
+        category: @category
+      )
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "shouldn't save when category is nil" do
+      @product = Product.new(
+        name: "Money Tree",
+        price_cents: 1499,
+        quantity: 50,
+        category: nil
+      )
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+    
   end
 end
