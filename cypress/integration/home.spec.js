e11c43ef076a722aa0d79a9ddb51950546754587
fayesadev <fayedumbrigue@gmail.com>
2023-03-14 23:58:06 +0000

    Add tests for homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+
+   it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
