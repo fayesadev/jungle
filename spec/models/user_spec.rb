35ead80eae05e8fe9e539da1d41307feb6220b20
fayesadev <fayedumbrigue@gmail.com>
2023-03-15 04:27:55 +0000

    Add user authentication tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,44 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    # validation tests/examples here
+    it "saves a new user when all fields are provided" do
+      @user = User.new(
+        name: "Billy",
+        email: "billyjoe@email.com",
+        password: "billyrocks",
+        password_confirmation: "billyrocks"
+      )
+      expect(@user).to be_valid
+    end
+
+    it "doesn't save a new user when password and confirmation don't match" do
+      @user = User.new(
+        name: "Billy",
+        email: "billyjoe@email.com",
+        password: "billyrocks",
+        password_confirmation: "billycks"
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "doesn't save a new user when name is nil" do
+      @user = User.new(
+        email: "billyjoe@email.com",
+        password: "billyrocks",
+        password_confirmation: "billycks"
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "doesn't save a new user when email is nil" do
+      @user = User.new(
+        name: "Billy",
+        password: "billyrocks",
+        password_confirmation: "billycks"
+      )
+      expect(@user).to_not be_valid
+    end
+end
